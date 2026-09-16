aba9dfa497131b6eccebf0e6874d240451b4160c
poatan2 <xorud211@naver.com>
2026-09-16 21:08:13 +0900

    first

---
+++ src/main/java/com/example/calculatorgradle/CalculatorGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculatorgradle/CalculatorGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
